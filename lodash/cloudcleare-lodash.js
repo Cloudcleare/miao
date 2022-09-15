2dcea418d2b32d43a914948f38cbe84eb728ef57
clear <a944120336@qq.com>
2022-09-15 22:43:55 +0800

    lodash

--- lodash/cloudcleare-lodash.js
+++ lodash/cloudcleare-lodash.js
@@ -9,9 +9,7 @@ var cloudcleare = {
         }
         arr.push(array[i])
     }
-    if(arr.length < size){
-      re.push(arr)
-    }
+    re.push(arr)
     return re
   },
 
@@ -40,6 +38,70 @@ var cloudcleare = {
       array.shift()
     }
     return array
+  },
+
+  flatten:function flatten (array) {
+    var result = []
+    for (var i = 0; i <array.length;i ++) {
+      if (!Array.isArray(array[i])){
+        result.push(array[i])
+      }else {
+        for (var j =0; j<array[i].length;j++){
+          result[i].push(array[j])
+        }
+      }
+    }
+    return result
+  },
+
+  flattenDeep:function flattenDeep(array) {
+    var result = []
+    if (!Array.isArray(array)){
+      result.push(array)
+    }else {
+      for (var i =0; i<arr.length;i++) {
+        result = result.concat(flattenDeep(array[i]))
+      }
+    }
+    return result
+  },
+
+  head:function head (array) {
+    return array[0]
+  },
+
+  indexOf:function indexOf (array, value, fromIndex=0) {
+    if (fromIndex < 0) {
+      for (var j = array.length -1; j >= 0;j --) {
+        if (value == array[j]) {
+          return j
+        }
+      }
+    }
+
+    for (var i = 0; i <array.length;i++){
+      if (value == array[j]) {
+        return i
+      }
+    }
+    return -1
+  },
+
+  lastIndexOf:function lastIndexOf(array, value, fromIndex=array.length-1) {
+    for (var i = fromIndex;i>=0;i--) {
+      if (array[i] == value){
+        return i
+      }
+    }
+    return -1
+  },
+
+  initial:function initial(array) {
+    var result =[]
+    for (var i = 0;i < array.length -2;i++){
+      result.push(array[i])
+    }
+    return result
   },
 
 
